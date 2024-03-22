b83b9119be100900ee4201308374a18a43273ec8
Jacob Dresdale <jacob.dresdale@quantcast.com>
2024-03-21 20:17:16 -0700

    revised

--- main.py
+++ main.py
@@ -1,110 +1,102 @@
 #!/usr/bin/env python3
-from methods.sonarr_api import SonarrAPI
+from methods.sonarr_api import Sonarr
 from pathlib import Path
 import os
 
 class Shows(object):
-    def __init__(self, config: object):
-        import json
-        import subprocess
-        self.tags = config.sonarr.get_tags()
-        self.series = config.sonarr.get_series()
-        self.repo = "https://github.com/manami-project/anime-offline-database.git"
-        process = subprocess.Popen(["git", "clone", self.repo], stdout=subprocess.PIPE)
-        output = process.communicate()[0]
-        config.log.info(f"Git clone output:\t{output}")
-        self.anidb = json.loads(open(Path(f"/config/anime-offline-database/anime-offline-database-minified.json")).read())["data"]
-        self.aggregate = list()
-        self.drop_tags = config.file["tagging"].get("drop", [])
-        self.replacement_tags = config.file["tagging"].get("replacements", {})
-        self.replacement_tags[" "] = "_" # this doesnt translate nice from a configmap so I injected it; besides tags don't accept spacing in sonarr
+	def __init__(self, config: object):
+		import json
+		import subprocess
+		self.tags = config.sonarr.get_tags()
+		self.series = config.sonarr.get_series()
+		self.repo = "https://github.com/manami-project/anime-offline-database.git"
+		process = subprocess.Popen(["git", "clone", self.repo], stdout=subprocess.PIPE)
+		output = process.communicate()[0]
+		config.log.info(f"Git clone output:\t{output}")
+		self.anidb = json.loads(open(Path(f"/config/anime-offline-database/anime-offline-database-minified.json")).read())["data"]
+		self.aggregate = list()
+		self.drop_tags = config.file["tagging"].get("drop", [])
+		self.replacement_tags = config.file["tagging"].get("replacements", {})
+		self.replacement_tags[" "] = "_" # this doesnt translate nice from a configmap so I injected it; besides tags don't accept spacing in sonarr
 
 class Show(object):
-    def __init__(self, show: dict):
-        self.title = show.get("title")
-        self.tags = unique(show.get("genres", []))
-        self.tag_ids = unique(show.get("tags", []))
-        self.id = show.get("id")
-        self.sonarr = dict()
-        self.type = show.get("seriesType")
+	def __init__(self, show: dict):
+		self.title = show.get("title")
+		self.tags = unique(show.get("genres", []))
+		self.tag_ids = unique(show.get("tags", []))
+		self.id = show.get("id")
+		self.sonarr = dict()
+		self.type = show.get("seriesType")
 
 class Config:
-    def __init__(self, config_file="/config/config.yaml"):
-        import yaml
-        import logging
-        self.file = yaml.load(open(config_file), Loader=yaml.FullLoader) if os.path.exists(Path(config_file)) else {"tagging": {"drop": [], "replacements": {}}}
-        self.log = logging
-        self.log.basicConfig(format='%(levelname)s:%(message)s', level=logging.INFO)
-        self.sonarr = SonarrAPI(url=os.environ["SONARR_URL"], apikey=os.environ["SONARR_API"])
-        self.shows = Shows(config=self)
-        self.parser()
+	def __init__(self, config_file="/config/config.yaml"):
+		import yaml
+		import logging
+		self.file = yaml.load(open(config_file), Loader=yaml.FullLoader) if os.path.exists(Path(config_file)) else {"tagging": {"drop": [], "replacements": {}}}
+		self.log = logging
+		self.log.basicConfig(format='%(levelname)s:%(message)s', level=logging.INFO)
+		self.sonarr = Sonarr(url=os.environ["SONARR_URL"], apikey=os.environ["SONARR_API"])
+		self.shows = Shows(config=self)
+		self.parser()
 
-    def parser(self):
-        for s in self.shows.series:
-            show = Show(s)
-            if show.type == "anime":
-                show.tags += ["anime", "animated", "animation", "japanese"]
-                for anime in self.shows.anidb:
-                    if anime["title"] == show.title:
-                        show.tags += anime["tags"]
-                        break
-            show.tags = unique([cleanup_tags(tag=i, replacements=self.shows.replacement_tags) for i in sorted(list(set(show.tags)))])
-            self.shows.aggregate.append(show)
-        self.write_tags()
+	def parser(self):
+		self.shows.tags = self.sonarr.get_tags()
+		previous_tags = self.shows.tags
+		for s in self.shows.series:
+			show = Show(s)
+			if show.type == "anime":
+				show.tags += ["anime", "animated", "animation", "japanese"]
+				for anime in self.shows.anidb:
+					if anime["title"] == show.title:
+						show.tags += anime["tags"]
+						break
+			show.tags = unique([cleanup_tags(tag=i, replacements=self.shows.replacement_tags) for i in sorted(list(set(show.tags)))])
+			self.shows.aggregate.append(show)
+			self.write_tags(show)
 
-    def write_tags(self):
-        self.shows.tags = self.sonarr.get_tags()
-        previous_tags = self.shows.tags
-        self.shows.tags = aggregate_tags(drop_tags=self.shows.drop_tags, input_tags=[show.tags for show in self.shows.aggregate])
-        for show in self.shows.aggregate:
-            self.log.info(f"Processing tags for {show.title}")
-            show.sonarr = [tvShow for tvShow in self.shows.series if tvShow["title"]==show.title][0]
-            add_tags(tags=aggregate_tags(drop_tags=self.shows.drop_tags, input_tags=show.tags), tagmap=self.sonarr.get_tags(), sonarr=self.sonarr)
-            try:
-                self.shows.tags = self.sonarr.get_tags()
-            except:
-                self.shows.tags = previous_tags
-            [show.tags.remove(i) for i in show.tags if i in self.shows.drop_tags]
-            show.tag_ids = unique([i.get("id") for i in self.shows.tags if (i.get("label") in show.tags)])
-            show.sonarr.update({"tags": show.tag_ids})
-            try:
-                self.log.info(f"Tagging has started for {show.title}:\t{show.tags}")
-                self.sonarr.update_series(series_id=show.id, data=show.sonarr)
-                self.log.info(f"Tagging has completed for {show.title}")
-            except:
-                pass
+	def write_tags(self, show):
+		self.log.info(f"Processing tags for {show.title}")
+		show.sonarr = [tvShow for tvShow in self.shows.series if tvShow["title"]==show.title][0]
+		add_tags(tags=aggregate_tags(drop_tags=self.shows.drop_tags, input_tags=show.tags), tagmap=self.sonarr.get_tags(), sonarr=self.sonarr)
+		try:
+			self.shows.tags = self.sonarr.get_tags()
+		except:
+			self.shows.tags = previous_tags
+		[show.tags.remove(i) for i in show.tags if i in self.shows.drop_tags]
+		show.tag_ids = unique([i.get("id") for i in self.shows.tags if (i.get("label") in show.tags)])
+		show.sonarr.update({"tags": show.tag_ids})
+		try:
+			self.log.info(f"Tagging has started for {show.title}:\t{show.tags}")
+			self.log.debug(self.sonarr.update_series(series_id=show.id, data=show.sonarr))
+			self.log.info(f"Tagging has completed for {show.title}")
+		except:
+			pass
 
 def cleanup_tags(tag: str, replacements: dict):
-    tag = tag.lower()
-    if tag.endswith("ss"):
-        tag = tag.rstrip(tag[-1])
-    for before, after in replacements.items():
-        tag = tag.replace(before, after)
-    return tag
+	tag = tag.lower()
+	if tag.endswith("ss"):
+		tag = tag.rstrip(tag[-1])
+	for before, after in replacements.items():
+		tag = tag.replace(before, after)
+	return tag
 
 def aggregate_tags(drop_tags: list, input_tags: list):
-    all_tags = list()
-    for tags in input_tags:
-        for tag in tags:
-            if tag not in all_tags:
-                all_tags.append(tag)
-    [all_tags.remove(i) for i in all_tags if i in drop_tags]
-    return unique(all_tags)
+	return unique([tag for tags in input_tags for tag in tags if tag not in drop_tags])
 
 def add_tags(tags: list, tagmap: object, sonarr: object):
-    for tag in tags:
-        for i in tagmap:
-            try:
-                if i.label == tag:
-                    sonarr.add_tag(tag)
-                    logging.info(f"+ success adding tag {tag}")
-                    break
-            except:
-                pass
+	for tag in tags:
+		for i in tagmap:
+			if i["label"] == tag:
+				try:
+					radarr.add_tag(tag)
+					logging.info(f"+ success adding tag {tag}")
+					break
+				except:
+					pass
 
 def unique(tags):
-    return sorted(list(set(tags)))
+	return sorted(list(set(tags)))
 
 
 if __name__ == "__main__":
-    config = Config()
+	config = Config()
--- methods/sonarr_api.py
+++ methods/sonarr_api.py
@@ -1,46 +1,55 @@
-import time
-import json
-import requests
+from pyarr.sonarr import SonarrAPI
+# import time
+# import json
+# import requests
 
 
-class SonarrAPI(object):
+class Sonarr(object):
 	def __init__(self, url: str, apikey: str):
-		self.host_url = url
-		self.api_key = apikey
+		self.api = RadarrAPI(host_url = url.replace("/api/v3", ""), api_key = apikey)
 
-	def sonarr_api_request(self, url, request_type = "get", data = dict()):
-		backoff_timer = 2
-		payload = json.dumps(data)
-		request_payload = dict()
-		if request_type not in ["post", "put", "delete"]:
-			request_payload = requests.get(url, headers = { 'X-Api-Key': self.api_key }, data = payload)
-		elif request_type == "put":
-			request_payload = requests.put(url, headers = { 'X-Api-Key': self.api_key }, data = payload)
-		elif request_type == "post":
-			request_payload = requests.post(url, headers = { 'X-Api-Key': self.api_key }, data = payload)
-		elif request_type == "delete":
-			request_payload = requests.delete(url, headers = { 'X-Api-Key': self.api_key }, data = payload)
-		time.sleep(backoff_timer)
-		return request_payload.json()
-
-	def get_root_folder(self):
-		return self.sonarr_api_request(f"{self.host_url}/rootfolder")
+	# def sonarr_api_request(self, url, request_type = "get", data = dict()):
+	# 	backoff_timer = 2
+	# 	payload = json.dumps(data)
+	# 	request_payload = dict()
+	# 	headers = {
+	# 		'X-Api-Key': self.api_key,
+	#         "Content-Type": "application/json",
+	#         "accept": "application/json",
+	# 	}
+	# 	if request_type not in ["post", "put", "delete"]:
+	# 		request_payload = requests.get(url, headers = headers, data = payload)
+	# 	elif request_type == "put":
+	# 		request_payload = requests.put(url, headers = headers, data = payload)
+	# 	elif request_type == "post":
+	# 		request_payload = requests.post(url, headers = headers, data = payload)
+	# 	elif request_type == "delete":
+	# 		request_payload = requests.delete(url, headers = headers, data = payload)
+	# 	time.sleep(backoff_timer)
+	# 	return request_payload.json()
 
 	def get_series(self):
-		return self.sonarr_api_request(f"{self.host_url}/series")
-
+		return self.api.get_series()
+	
 	def update_series(self, series_id, data: dict):
-		return self.sonarr_api_request(f"{self.host_url}/series/{series_id}", "put", data)
-
-	def refresh_series(self, series_id, data = dict()):
-		return self.sonarr_api_request(f"{self.host_url}/command/RefreshSeries&seriesId={series_id}", "post", data)
-
-	def rescan_series(self, series_id, data = dict()):
-		return self.sonarr_api_request(f"{self.host_url}/command/RescanSeries&seriesId={series_id}", "post", data)
-
+		return self.api.upd_series(data)
+	
 	def get_tags(self):
-		return self.sonarr_api_request(f"{self.host_url}/tag")
-
+		return self.api.get_tag()
+	
 	def add_tag(self, tag: str):
-		data = json.dumps({ "label" : tag })
-		return self.sonarr_api_request(url=f"{self.host_url}/tag", request_type="post", data=data)
+		return self.api.create_tag(label=tag)
+
+
+	# def get_series(self):
+	# 	return self.sonarr_api_request(f"{self.host_url}/series")
+
+	# def update_series(self, series_id, data: dict):
+	# 	return self.sonarr_api_request(f"{self.host_url}/series/{series_id}", "put", data)
+
+	# def get_tags(self):
+	# 	return self.sonarr_api_request(f"{self.host_url}/tag")
+
+	# def add_tag(self, tag: str):
+	# 	data = json.dumps({ "label" : tag })
+	# 	return self.sonarr_api_request(url=f"{self.host_url}/tag", request_type="post", data=data)
